1f3843cf98b2421e36e28ff70cd32cf10ec7ecab
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-02-15 14:16:05 +0100

    Radical cut-down of former Table implementation.

--- packages/table/Table.js
+++ packages/table/Table.js
@@ -1,84 +1,30 @@
 'use strict';
 
 var isArray = require('lodash/isArray');
-var extend = require('lodash/extend');
+var cloneDeep = require('lodash/cloneDeep');
 var oo = require('../../util/oo');
 var BlockNode = require('../../model/BlockNode');
-var ParentNodeMixin = require('../../model/ParentNodeMixin');
-var TableMatrix = require('./TableMatrix');
-var TableCellIterator = require('./TableCellIterator');
 var uuid = require('../../util/uuid');
 
 function Table() {
   Table.super.apply(this, arguments);
-
-  this.matrix = null;
 }
 
 Table.Prototype = function () {
 
-  extend(this, ParentNodeMixin);
-
-  this.hasChildren = function() {
-    return false;
-  };
-
-  this.getChildrenProperty = function() {
-    return 'sections';
-  };
-
-  this.getSections = function() {
-    return this.getChildren();
-  };
-
-  this.getSectionAt = function(secIdx) {
-    return this.getChildAt(secIdx);
-  };
-
-  this.getMatrix = function() {
-    if (!this.matrix) {
-      this.matrix = new TableMatrix(this);
-      this.matrix.update();
-    }
-    return this.matrix;
-  };
-
-  /*
-   * Provides a cell iterator that allows convenient traversal regardless of
-   * the structure with respect to sections.
-   *
-   * @returns {TableCellIterator}
-   */
-  this.getIterator = function() {
-    return new TableCellIterator(this);
-  };
-
   this.getSize = function(dimension) {
-    var dim = this.matrix.getSize();
+    var nrows = this.cells.length;
+    var ncols = 0;
+    if (nrows > 0) {
+      ncols = this.cells[0].length;
+    }
     if ( dimension === 'row' ) {
-      return dim[0];
+      return nrows;
     } else if ( dimension === 'col' ) {
-      return dim[1];
+      return ncols;
     } else {
-      return dim;
+      return [nrows, ncols];
     }
-  };
-
-  this.toTSV = function() {
-    var cell;
-    var data = [];
-    var row = [];
-    var it = this.getIterator();
-    it.onNewRow = function() {
-      row = [];
-      data.push(row);
-    };
-    while( (cell = it.next()) ) {
-      row.push(cell.content);
-    }
-    return data.map(function(row) {
-      return row.join('\t');
-    }).join('\n');
   };
 
 };
@@ -88,161 +34,24 @@ oo.inherit(Table, BlockNode);
 Table.static.name = "table";
 
 Table.static.defineSchema({
-  "sections": { type: ["id"], 'default': [] }
+  "cells": { type: ["array", "array", "string"], 'default': [] }
 });
 
-Table.fromTSV = function(tx, tsv, sep) {
-  sep = sep || '\t';
-  var lines = tsv.split(/\n/);
-  var tableId = uuid('table');
-  var sectionData = {
-    type: 'table-section',
-    id: tableId+'-body',
-    sectionType: 'tbody',
-    rows: [],
-    parent: tableId,
-  };
-  for (var i = 0; i < lines.length; i++) {
-    var rowData = {
-      type: 'table-row',
-      id: tableId + '-row'+i,
-      cells: [],
-      parent: sectionData.id,
-    };
-    var cells = lines[i].split(sep);
-    for (var j = 0; j < cells.length; j++) {
-      var cellStr = cells[j];
-      var cellData = {
-        type: 'table-cell',
-        id: rowData.id + '-col' + j,
-        cellType: 'td',
-        content: cellStr,
-        parent: rowData.id,
-      };
-      var cell = tx.create(cellData);
-      rowData.cells.push(cell.id);
-    }
-    var row = tx.create(rowData);
-    sectionData.rows.push(row.id);
-  }
-  var section = tx.create(sectionData);
-  return tx.create({
-    type: 'table',
-    id: tableId,
-    sections: [section.id]
-  });
-};
-
 Table.create = function(tx, options) {
-  options = options || {};
-  var addHeader = options.header || false;
-  var vals, rowVals;
-  var nrows, ncols;
-  if (isArray(options.vals) && options.vals.length > 0) {
-    vals = options.vals;
-    nrows = vals.length;
-    ncols = vals[0].length;
-  } else {
-    vals = [];
-    nrows = options.rows || 5;
-    ncols = options.cols || 10;
-  }
-
-  var tableData, sectionData, rowData, cellData;
-  var tableId, sectionId, rowId, cellId;
-  var cellStr;
-  var i,j;
-
-  tableId = uuid('table');
-  tableData = {
+  var tableData = {
+    id: uuid('table'),
     type: 'table',
-    id: tableId,
-    sections: []
+    cells: []
   };
-
-  if (addHeader) {
-    if (vals.length > 0) {
-      rowVals = vals.shift();
-      nrows--;
-    } else {
-      rowVals = [];
+  if (isArray(options.vals)) {
+    tableData.cells = cloneDeep(options.vals);
+  } else {
+    var nrows = options.rows || 5;
+    var ncols = options.cols || 10;
+    for(var row = 0; row < nrows; row++) {
+      tableData.cells.push(new Array(ncols));
     }
-    sectionId = tableId+'-head';
-    sectionData = {
-      type: 'table-section',
-      id: sectionId,
-      sectionType: 'thead',
-      rows: [],
-      parent: tableId,
-    };
-    rowId = sectionId + '-row';
-    rowData = {
-      type: 'table-row',
-      id: rowId,
-      cells: [],
-      parent: sectionId,
-    };
-    sectionData.rows.push(rowId);
-    for (j = 0; j < ncols; j++) {
-      cellStr = rowVals[j] || Table.getIdForCoordinate(0, j+1);
-      cellId = rowId + 'col' + j;
-      cellData = {
-        type: 'table-cell',
-        id: cellId,
-        cellType: 'th',
-        content: cellStr,
-        parent: rowId,
-      };
-      tx.create(cellData);
-      rowData.cells.push(cellId);
-    }
-    tx.create(rowData);
-    tx.create(sectionData);
-    tableData.sections.push(sectionId);
   }
-
-  // body section
-  sectionId = tableId+'-body';
-  sectionData = {
-    type: 'table-section',
-    id: sectionId,
-    sectionType: 'tbody',
-    rows: [],
-    parent: tableId,
-  };
-  for (i = 0; i < nrows; i++) {
-    if (vals.length > 0) {
-      rowVals = vals.shift();
-      nrows--;
-    } else {
-      rowVals = [];
-    }
-    rowId = sectionId + '-row'+i;
-    rowData = {
-      type: 'table-row',
-      id: rowId,
-      cells: [],
-      parent: sectionId,
-    };
-    for (j = 0; j < ncols; j++) {
-      cellStr = rowVals[j] || '';
-      cellId = rowId + '-col' + j;
-      cellData = {
-        type: 'table-cell',
-        id: cellId,
-        cellType: 'td',
-        content: cellStr,
-        parent: rowId,
-      };
-      tx.create(cellData);
-      rowData.cells.push(cellId);
-    }
-    tx.create(rowData);
-    sectionData.rows.push(rowId);
-  }
-  tx.create(sectionData);
-  tableData.sections.push(sectionId);
-
   return tx.create(tableData);
 };
 
--- packages/table/TableComponent.js
+++ packages/table/TableComponent.js
@@ -1,29 +1,13 @@
 'use strict';
 
-var each = require('lodash/each');
-var $ = require('../../util/jquery');
 var Component = require('../../ui/Component');
-var TextProperty = require('../../ui/TextPropertyComponent');
-var TableSelection = require('../../model/TableSelection');
 var $$ = Component.$$;
 
 function TableComponent() {
   Component.apply(this, arguments);
-
-  this.context.surface.connect(this, {
-    "selection:changed": this.onSelectionChange
-  });
 }
 
 TableComponent.Prototype = function() {
-
-  this.dispose = function() {
-    this.context.surface.disconnect(this);
-  };
-
-  this.getInitialState = function() {
-    return { mode: 'table' };
-  };
 
   this.render = function() {
     var tableEl = $$('table')
@@ -32,172 +16,32 @@ TableComponent.Prototype = function() {
         "data-id": this.props.node.id,
         contentEditable: false
       });
-    if (this.state.mode === "table") {
-      tableEl.addClass('table-editing-mode');
-    } else if (this.state.mode === "cell") {
-      tableEl.addClass('cell-editing-mode');
-    }
-    // HACK: make sure row col indexes are up2date
-    this.props.node.getMatrix();
     tableEl.append(this.renderTableContent());
     return tableEl;
   };
 
   this.renderTableContent = function() {
-    var content = [];
-    each(this.props.node.getSections(), function(sec) {
-      var secEl = $$("t"+sec.sectionType).key(sec.id);
-      each(sec.getRows(), function(row) {
-        var rowEl = $$("tr").attr({"data-id": row.id, contentEditable:false});
-        each(row.getCells(), function(cell) {
-          var cellEl = $$((cell.cellType === 'head') ? 'th' : 'td')
-            .attr({
-              "data-id": cell.id,
-              "data-row": cell.rowIdx,
-              "data-col": cell.colIdx,
-            })
-            .on('mousedown', this.onMouseDown)
-            .on('doubleclick', this.onDoubleClick);
-          if (cell.colspan) {
-            cellEl.attr("colspan", cell.colspan);
-          }
-          if (cell.rowspan) {
-            cellEl.attr('rowspan', cell.rowspan);
-          }
-          if (this.state.mode === "cell") {
-            if (cell.rowIdx === this.state.row && cell.colIdx === this.state.col) {
-              cellEl.attr('contentEditable', true);
-            }
-          }
-          cellEl.append($$(TextProperty, {
-            path: [ cell.id, "content"]
-          }));
-          rowEl.append(cellEl);
-        }.bind(this));
-        secEl.append(rowEl);
-      }.bind(this));
-      content.push(secEl);
-    }.bind(this));
-    return content;
-  };
-
-  this.onDoubleClick = function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    var surface = this.context.surface;
-    var $cell = $(e.currentTarget);
-    var cellId = $cell.data('id');
-    // switch the state
-    this.setState({
-      mode: "cell",
-      cellId: cellId,
-      row: $cell.data('row'),
-      col: $cell.data('col')
-    }, function() {
-      var doc = surface.getDocument();
-      var path = [cellId, 'content'];
-      var text = doc.get(path);
-      surface.setSelection({
-        type: 'property',
-        path: path,
-        startOffset: text.length,
-      });
-    });
-  };
-
-  this.onMouseDown = function(e) {
-    var $el = this.$el;
-    var surface = this.context.surface;
-    var doc = surface.getDocument();
-    var id = this.props.node.id;
-    var self = this;
-    // 1. get the anchor cell (for click or select)
-    var $cell = $(e.currentTarget);
-
-    if (this.state.mode === "cell" && this.state.cellId === $cell.data('id')) {
-      // do not override selection behavior within a cell
-      return;
-    }
-    e.preventDefault();
-    e.stopPropagation();
-
-    var $startCell = $cell;
-    var $endCell = $cell;
-    var rectangle = new TableSelection.Rectangle(
-      $startCell.data('row'),
-      $startCell.data('col'),
-      $endCell.data('row'),
-      $endCell.data('col')
-    );
-    // 2. enable onMouseOver delegate which updates the displayed selection region
-    var onMouseOver = function(e) {
-      $endCell = $(e.currentTarget);
-      rectangle = new TableSelection.Rectangle($startCell.data('row'), $startCell.data('col'),
-        $endCell.data('row'), $endCell.data('col'));
-      self._updateSelection(rectangle);
-    };
-    $el.on('mouseenter', 'th,td', onMouseOver);
-    // 3. enable onMouseUp hook: stop dragging the selection and
-    // finally set the Surface's selection
-    $(window).one('mouseup', function(e) {
-      e.stopPropagation();
-      e.preventDefault();
-      $el.off('mouseenter', 'th,td', onMouseOver);
-      var tableSelection = doc.createSelection({
-        type: 'table',
-        tableId: id,
-        rectangle: rectangle
-      });
-      surface.setSelection(tableSelection);
-    });
-  };
-
-  this.onSelectionChange = function(sel) {
     var node = this.props.node;
-    var id = node.id;
-    if (this.hasSelection) {
-      this._clearSelection();
+    var cells = node.cells;
+    var content = [];
+    for(var rowIdx = 0; rowIdx < cells.length; rowIdx++) {
+      var row = cells[rowIdx];
+      var rowEl = $$("tr");
+      for (var colIdx = 0; colIdx < row.length; colIdx++) {
+        var cell = row[colIdx];
+        // ATTENTION: table model is sparse,
+        // i.e. empty cells are just null
+        var cellEl = $$('td');
+        if (cell) {
+          cellEl.append(cell);
+        } else {
+          cellEl.append(' ');
+        }
+        rowEl.append(cellEl);
+      }
+      content.push(rowEl);
     }
-    if (this.state.mode === "cell") {
-      if (!sel.isPropertySelection() || sel.start.path[0] !== this.state.cellId) {
-        var self = this;
-        this.setState({
-          mode: "table"
-        }, function() {
-          if (sel.isTableSelection() && sel.getTableId() === id) {
-            self._updateSelection(sel.rectangle);
-          }
-        });
-      }
-    } else {
-      if (sel.isTableSelection() && sel.getTableId() === id) {
-        this._updateSelection(sel.rectangle);
-      }
-    }
-  };
-
-  this._updateSelection = function(rectangle) {
-    var $el = this.$el;
-    var $cells = $el.find('th,td');
-    $cells.each(function() {
-      var $cell = $(this);
-      var row = $cell.data('row');
-      var col = $cell.data('col');
-      if (row >= rectangle.start.row && row <= rectangle.end.row &&
-        col >= rectangle.start.col && col <= rectangle.end.col) {
-        $cell.addClass("selected");
-      } else {
-        $cell.removeClass("selected");
-      }
-    });
-    this.hasSelection = true;
-  };
-
-  this._clearSelection = function() {
-    var $el = this.$el;
-    var $cells = $el.find('th,td');
-    $cells.removeClass('selected');
-    this.hasSelection = false;
+    return content;
   };
 };
 
